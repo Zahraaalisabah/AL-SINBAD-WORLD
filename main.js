b56bf5b4a04add8eea632b3b4b5ca1cd798ff3da
Zahraaalisabah <129872442+Zahraaalisabah@users.noreply.github.com>
2026-09-16 10:59:13 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
-const SUPABASE_URL = "https://oolthhsdewchdkuojahl.supabase.co";
 
-const SUPABASE_KEY = "sb_publishable_K9MBG_wuPlWY25Lv5lEpJQ_wph27Xs";
-
-const supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
 // ==============================
 // AOS Animation Init
 // ==============================
